395d0e1fe8eff08ae7d7da326d83e3174c4c3440
mribr <mribrahim229@gmail.com>
2024-02-12 17:02:28 +0300

    t

---
+++ Sorter.java
@@ -0,0 +1,15 @@
+public class InsertionSort {
+    public static void main(String[] args){
+        System.out.println("Hello");
+
+
+
+    }
+    public static void insertionSort(){
+
+    }
+    public static void sort(){
+        insertionSort();
+
+    }
+}
